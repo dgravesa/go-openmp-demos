fcb19f1670d8a6aa50e66b5a5819e10cfd5dced0
Daniel Graves <dgravesa@gmail.com>
2021-12-12 23:57:19 -0500

    creating some common pre and post loop logic functions, migrating 2nd go code

--- demos/go-2-all-goroutines/main.go
+++ demos/go-2-all-goroutines/main.go
@@ -1,66 +1,25 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"math"
-	"math/rand"
-	"runtime"
 	"sync"
-	"time"
+
+	"github.com/dgravesa/go-openmp/demos/go-common"
 )
 
 func main() {
-	var N int
-	var seed int64
-	var printSome int
-	var numCPU int
-
-	flag.IntVar(&N, "N", 10000000, "number of work items")
-	flag.Int64Var(&seed, "Seed", 1, "random generator seed")
-	flag.IntVar(&printSome, "PrintSome", 0, "print first num values of result to verify")
-	flag.IntVar(&numCPU, "NumCPU", runtime.NumCPU(), "number of CPUs to use")
-	flag.Parse()
-
-	runtime.GOMAXPROCS(numCPU)
-
-	// initialize input array of N values
-	rand.Seed(seed)
-	inputArray := make([]float64, N)
-	for i := 0; i < N; i++ {
-		inputArray[i] = 10 * (rand.Float64() - 0.5) // -5 to 5
-	}
-
-	// allocate output array
-	outputArray := make([]float64, N)
+	common.PreLoop()
 
 	// execute loop
-	startTime := time.Now()
 	var wg sync.WaitGroup
-	wg.Add(N)
-	for i := 0; i < N; i++ {
+	wg.Add(common.N)
+	for i := 0; i < common.N; i++ {
 		go func(i int) {
 			defer wg.Done()
-			outputArray[i] = sinc(inputArray[i] * math.Pi)
+			common.Results[i] = common.Sinc(common.Inputs[i] * math.Pi)
 		}(i)
 	}
 	wg.Wait()
-	stopTime := time.Now()
 
-	// print execution time
-	totalLoopDuration := stopTime.Sub(startTime)
-	fmt.Println(totalLoopDuration)
-
-	// print some output values
-	if printSome > 0 {
-		fmt.Println("inputs:", inputArray[:printSome])
-		fmt.Println("outputs:", outputArray[:printSome])
-	}
-}
-
-func sinc(x float64) float64 {
-	if x == 0.0 {
-		return 1.0
-	}
-	return math.Sin(x) / x
+	common.PostLoop()
 }
---
+++ demos/go-common/common.go
@@ -0,0 +1,87 @@
+package common
+
+import (
+	"flag"
+	"fmt"
+	"math"
+	"math/rand"
+	"runtime"
+	"time"
+)
+
+var Params struct {
+	NumCPU int
+
+	seed        int64
+	printSome   int
+	runSerial   bool
+	traceName   string
+	cpuProfName string
+}
+
+var (
+	N              int
+	NumCPU         int
+	Inputs         []float64
+	Results        []float64
+	loopStartTime  time.Time
+	loopFinishTime time.Time
+)
+
+func parseFlags() {
+	flag.IntVar(&N, "N", 1000000, "number of work items")
+	flag.IntVar(&NumCPU, "numgr", runtime.NumCPU(), "number of goroutines to use in parallel loop")
+	flag.Int64Var(&Params.seed, "seed", 1, "random generator seed")
+	flag.IntVar(&Params.printSome, "printsome", 0, "print first num values of result to verify")
+	flag.BoolVar(&Params.runSerial, "serial", false, "run non-parallelized for loop instead")
+	flag.StringVar(&Params.traceName, "trace", "", "output trace of loop to file")
+	flag.StringVar(&Params.cpuProfName, "cpuprofile", "", "output CPU profile of loop to file")
+	flag.Parse()
+
+	runtime.GOMAXPROCS(Params.NumCPU)
+}
+
+func initArrays() {
+	// initialize input array of N values
+	rand.Seed(Params.seed)
+	Inputs = make([]float64, N)
+	for i := 0; i < N; i++ {
+		Inputs[i] = 10 * (rand.Float64() - 0.5) // -5 to 5
+	}
+
+	// allocate output array
+	Results = make([]float64, N)
+}
+
+func printResults() {
+	// print execution time
+	totalLoopDuration := loopFinishTime.Sub(loopStartTime)
+	fmt.Println(totalLoopDuration)
+
+	// print some output values
+	if Params.printSome > 0 {
+		fmt.Println("inputs:", Inputs[:Params.printSome])
+		fmt.Println("outputs:", Results[:Params.printSome])
+	}
+}
+
+func PreLoop() {
+	parseFlags()
+
+	initArrays()
+
+	loopStartTime = time.Now()
+}
+
+func PostLoop() {
+	loopFinishTime = time.Now()
+
+	printResults()
+}
+
+func Sinc(x float64) float64 {
+	if x == 0.0 {
+		return 1.0
+	}
+	return math.Sin(x) / x
+}
